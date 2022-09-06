eeea76cf94510aa20ff82e8e16f9997acf8cf812
LaercioPaiva <esselaerciopaiva@gmail.com>
2022-09-06 15:17:39 -0300

    projeto senac

---
+++ PI/site-consumer/js/models.js
@@ -0,0 +1,9 @@
+
+let modelsJson = [
+    {id:1, name:'Banana', img:'img/banana.png', price:[4.50, 8.50, 12.50], sizes:['1kg', '2kg', '3kg'], description:'Banana Nanica rica em vitaminas do complexo B, possuem cerca de 90Kcal a cada 100g, produzida de maneira organica em Schroeder/SC'},
+    {id:2, name:'Repolho', img:'img/repolho.png', price:[3.50, 6.50, 11.00], sizes:['1kg', '2kg', '3kg'], description:'Aqui vai a descricao'},
+    {id:3, name:'Tomate', img:'img/tomate.png', price:[3.20, 6.00, 9.00], sizes:['1kg', '2kg', '3kg'], description:'Aqui vai a descricao'},
+    {id:4, name:'Beringela', img:'img/beringela.png', price:[3.99, 6.48, 10.00], sizes:['1kg', '2kg', '3kg'], description:'Aqui vai a descricao'},
+    {id:5, name:'Alface', img:'img/alface.png', price:[3.20, 6.00, 9.00], sizes:['1kg', '2kg', '3kg'], description:'Aqui vai a descricao'},
+    {id:6, name:'Batata', img:'img/batata.png', price:[3.50, 6.50, 11.00], sizes:['1', '2', '3'], description:'Aqui vai a descricao'},
+];
