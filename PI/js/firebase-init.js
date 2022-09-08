a209596a2a930b6e5226f10729066d1a6b276d0b
LaercioPaiva <esselaerciopaiva@gmail.com>
2022-09-08 16:18:46 -0300

    autenticacao via firebase feita

---
+++ PI/js/firebase-init.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB81jxJSt2Tj686OtobbeMiYRWoQ8K1AIM",
+    authDomain: "senac-30e57.firebaseapp.com",
+    projectId: "senac-30e57",
+    storageBucket: "senac-30e57.appspot.com",
+    messagingSenderId: "184453984954",
+    appId: "1:184453984954:web:2c0b8fddc0ab963dcec883",
+    measurementId: "G-KRTLYY0K2Z"
+};
+firebase.initializeApp(firebaseConfig);
