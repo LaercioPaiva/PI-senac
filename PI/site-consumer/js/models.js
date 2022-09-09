65b38d8f2fc22165132a18319cad3b3f3125b9e0
Renan Becker <51799627+RenanBecker@users.noreply.github.com>
2022-09-09 13:20:49 -0300

    correção ortográfica

--- PI/site-consumer/js/models.js
+++ PI/site-consumer/js/models.js
@@ -4,6 +4,6 @@ let modelsJson = [
     {id:2, name:'Repolho', img:'img/repolho.png', price:[3.50, 6.50, 11.00], sizes:['1kg', '2kg', '3kg'], description:'Repolho rico em antioxidantes com efeitos antí-inflamatórios, produzidos no Distrito de Pirabeiraba'},
     {id:3, name:'Tomate', img:'img/tomate.png', price:[3.20, 6.00, 9.00], sizes:['1kg', '2kg', '3kg'], description:'Tomate rico em vitaminas C, A e K, potássio e licopeno com alta propriedade antioxidantequi, produzidos em Guaramirin/SC '},
     {id:4, name:'Beringela', img:'img/beringela.png', price:[3.99, 6.48, 10.00], sizes:['1kg', '2kg', '3kg'], description:'Beringela rica em vitaminas A, B1, B2, B5 e C, produzidos de maneira orgânica em Massaranduba/SC'},
-    {id:5, name:'Alface', img:'img/alface.png', price:[3.20, 6.00, 9.00], sizes:['1Un', '2Un', '3Un'], description:'Alface rico em vimanima A e de baixa caloria, apenas 15 calorias por 100 gramas, produzido em horta comunitária em Pomerode/SC'},
-    {id:6, name:'Batata', img:'img/batata.png', price:[3.50, 6.50, 11.00], sizes:['1Kg', '2Kg', '3Kg'], description:'Batata, alimento rico em fósfoto, ferro, potássio e cálcio, produzida em Mafra/SC'},
+    {id:5, name:'Alface', img:'img/alface.png', price:[3.20, 6.00, 9.00], sizes:['1Un', '2Un', '3Un'], description:'Alface rico em vimanima A e de baixa caloria, apenas 15kcal a cada 100g, produzido em horta comunitária em Pomerode/SC'},
+    {id:6, name:'Batata', img:'img/batata.png', price:[3.50, 6.50, 11.00], sizes:['1Kg', '2Kg', '3Kg'], description:'Batata, alimento rico em fósforo, ferro, potássio e cálcio, produzida em Mafra/SC'},
 ];
