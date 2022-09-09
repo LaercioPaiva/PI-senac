2862ca294d1468078e18ed41e1635720ff76e639
Renan Becker <51799627+RenanBecker@users.noreply.github.com>
2022-09-09 13:18:30 -0300

    Adicionado descrição dos itens

--- PI/site-consumer/js/models.js
+++ PI/site-consumer/js/models.js
@@ -1,9 +1,9 @@
 
 let modelsJson = [
-    {id:1, name:'Banana', img:'img/banana.png', price:[4.50, 8.50, 12.50], sizes:['1kg', '2kg', '3kg'], description:'Banana Nanica rica em vitaminas do complexo B, possuem cerca de 90Kcal a cada 100g, produzida de maneira organica em Schroeder/SC'},
-    {id:2, name:'Repolho', img:'img/repolho.png', price:[3.50, 6.50, 11.00], sizes:['1kg', '2kg', '3kg'], description:'Aqui vai a descricao'},
-    {id:3, name:'Tomate', img:'img/tomate.png', price:[3.20, 6.00, 9.00], sizes:['1kg', '2kg', '3kg'], description:'Aqui vai a descricao'},
-    {id:4, name:'Beringela', img:'img/beringela.png', price:[3.99, 6.48, 10.00], sizes:['1kg', '2kg', '3kg'], description:'Aqui vai a descricao'},
-    {id:5, name:'Alface', img:'img/alface.png', price:[3.20, 6.00, 9.00], sizes:['1kg', '2kg', '3kg'], description:'Aqui vai a descricao'},
-    {id:6, name:'Batata', img:'img/batata.png', price:[3.50, 6.50, 11.00], sizes:['1', '2', '3'], description:'Aqui vai a descricao'},
+    {id:1, name:'Banana', img:'img/banana.png', price:[4.50, 8.50, 12.50], sizes:['1kg', '2kg', '3kg'], description:'Banana Nanica rica em vitaminas do complexo B, possuem cerca de 90Kcal a cada 100g, produzida de maneira orgânica em Schroeder/SC'},
+    {id:2, name:'Repolho', img:'img/repolho.png', price:[3.50, 6.50, 11.00], sizes:['1kg', '2kg', '3kg'], description:'Repolho rico em antioxidantes com efeitos antí-inflamatórios, produzidos no Distrito de Pirabeiraba'},
+    {id:3, name:'Tomate', img:'img/tomate.png', price:[3.20, 6.00, 9.00], sizes:['1kg', '2kg', '3kg'], description:'Tomate rico em vitaminas C, A e K, potássio e licopeno com alta propriedade antioxidantequi, produzidos em Guaramirin/SC '},
+    {id:4, name:'Beringela', img:'img/beringela.png', price:[3.99, 6.48, 10.00], sizes:['1kg', '2kg', '3kg'], description:'Beringela rica em vitaminas A, B1, B2, B5 e C, produzidos de maneira orgânica em Massaranduba/SC'},
+    {id:5, name:'Alface', img:'img/alface.png', price:[3.20, 6.00, 9.00], sizes:['1Un', '2Un', '3Un'], description:'Alface rico em vimanima A e de baixa caloria, apenas 15 calorias por 100 gramas, produzido em horta comunitária em Pomerode/SC'},
+    {id:6, name:'Batata', img:'img/batata.png', price:[3.50, 6.50, 11.00], sizes:['1Kg', '2Kg', '3Kg'], description:'Batata, alimento rico em fósfoto, ferro, potássio e cálcio, produzida em Mafra/SC'},
 ];
